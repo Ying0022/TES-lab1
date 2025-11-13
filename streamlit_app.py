0a21016303e2209660f79611734da7ae0e00a891
YapPeiYing <yappy-wm22@student.tarc.edu.my>
2025-11-13 15:21:03 +0800

    formula

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,10 @@ st.divider()  # 👈 Draws a horizontal rule
 st.write("This text is between the horizontal rules.")
 
 st.divider()  # 👈 Another horizontal rule
+
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
